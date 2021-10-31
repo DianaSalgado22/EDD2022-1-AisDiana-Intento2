307846a023aac61053f65efc31deca560cd400a3
DianaSalgado22 <dianasalgado@ciencias.unam.mx>
2021-10-31 09:53:40 -0700

    Avance clase Maze

---
+++ Practica03/src/fciencias/edatos/practica03/Maze.java
@@ -0,0 +1,45 @@
+package fciencias.edatos.practica03;
+
+import java.util.Iterator;
+
+/**
+ * @version 1.0 Octubre 2021.
+ * @author Salgado Tirado Diana Laura
+ * @author Celic Aislinn Liahut Ley
+ * @since Estructuras de Datos 2022-1
+ */
+
+public class Maze{
+
+    /** Un arreglo de casillas 
+     *  que es la representación del 
+     *  tablero del laberinto */
+    Box[][] tablero;  
+
+    /** Representa la casilla de inicio del
+     *  recorrido del laberinto.
+     *  Esta casilla no puede ser una pared*/
+    Box inicio;
+
+    /** Representa la casilla de fin del
+     *  recorrido del laberinto.*/
+    Box fin;
+
+    /** Representa la casilla actual del
+     *  recorrido del laberinto.
+     *  Esta casilla no puede ser una pared*/
+    Box actual;
+
+    /** Metodo para saber si el laberinto esta resuelto
+     *  @return true si ya esta resuleto,false si no.
+     */
+    public boolean isSolution{
+        // Si son iguales significa que encontramos un camino
+        if(actual==fin)
+           return isExtensible();
+        // De otra manera aún no se encuentra una solución
+        return false
+    }
+
+
+}
