8c1d3d93c558ab4dc9b11d3293546ee6f4bb7cbb
DianaSalgado22 <dianasalgado@ciencias.unam.mx>
2021-11-05 12:09:54 -0700

    Se hizo más corto el método mazoCompleto

--- Proyecto1/src/fciencias/edatos/proyecto01/Mazo.java
+++ Proyecto1/src/fciencias/edatos/proyecto01/Mazo.java
@@ -13,161 +13,30 @@ public class Mazo{
     // Un mazo es una lista de cartas
     // Un mazo tiene 52 cartas
 
-    //INTENTO DE OPTIMIZACIÓN FALLIDO
-    /** Metodo para construir un mazoCompleto,
-     *  es decir con las 52 cartas de una baraja
-     *  inglesa. Este mazo ya estaria barajeado.
-     *  @return Una lista de 52 cartas diferentes.
-     */
-    //  public TDAList<Carta> mazoCompleto(){
-    //     // Creamos la lista de cartas
-    //     TDAList<Carta> mazo= new DoubleLinkedList<>();
-    //     //
-    //     TDAList<String> agregados= new DoubleLinkedList<>();
-    //     // Objetos Random para poder elegir números al azar
-    //     Random random1= new Random();
-    //     Random random2=new Random();
-    //     String palo="";
-    //     int valor=0;
-    //     String paraBuscar=valor+palo;
-    //     // iterador para seguir creando cartas hasta que nuestro mazo este completo
-    //     while(mazo.size()!=5){
-    //         while(agregados.contains(paraBuscar)){
-    //         int paloNum=0; // un numero pra poder usar random y asignar el palo a la cartas
-    //         /* Este while es necesario porque nextInt 
-    //          * crea valores desde 0 y necesitamos 
-    //          * que empiecen en 1. */
-    //         while(valor==0 || paloNum==0){ 
-    //         valor= random1.nextInt(13); // genera un numero random entre [0,13)
-    //         paloNum= random2.nextInt(5); // genera un numero random entre [0,5)
-    //         }
-    //         /* En este switch asignaremos al numero 
-    //          * generado random su determinado valor en String*/
-    //         switch(paloNum){
-    //             case 1:
-    //                 palo= "Picas";
-    //                 break;
-    //             case 2:
-    //                 palo= "Corazones";
-    //                 break;
-    //             case 3:
-    //                 palo= "Diamantes";
-    //                 break;
-    //             case 4:
-    //                 palo= "Treboles";
-    //                 break;
-    //         }
-    //     }
-            
-    //         Carta carta= new Carta(palo,valor);
-    //         mazo.add(0,carta);
-    //         agregados.add(0,paraBuscar);
-            
-    //     }
-    //     return mazo; // Se regresa el mazo  ya completo
-    // } 
-
-    // FUNCIONA PERO ES LENTISIMO CUANDO PONES 52 CARTAS
-    /** Metodo para construir un mazoCompleto,
-     *  es decir con las 52 cartas de una baraja
-     *  inglesa. Este mazo ya estaria barajeado.
-     *  @return Una lista de 52 cartas diferentes.
-     */
-    //      public TDAList<Carta> mazoCompleto(){
-    //     // Creamos la lista de cartas
-    //     TDAList<Carta> mazo= new DoubleLinkedList<>();
-    //     // En este String se almacenas las cartas ya agregadas
-    //     String cartasAgregadas="";
-    //     // Objetos Random para poder elegir números al azar
-    //     Random random1= new Random();
-    //     Random random2=new Random();
-    //     // iterador para seguir creando cartas hasta que nuestro mazo este completo
-    //     while(mazo.size()!=52){
-    //         int valor=0; // el valor que tendra la carta
-    //         int paloNum=0; // un numero pra poder usar random y asignar el palo a la cartas
-    //         /* Este while es necesario porque nextInt 
-    //          * crea valores desde 0 y necesitamos 
-    //          * que empiecen en 1. */
-    //         while(valor==0 || paloNum==0){ 
-    //         valor= random1.nextInt(13); // genera un numero random entre [0,13)
-    //         paloNum= random2.nextInt(5); // genera un numero random entre [0,5)
-    //     }
-    //         String palo=""; 
-    //         /* En este switch asignaremos al numero 
-    //          * generado random su determinado valor en String*/
-    //         switch(paloNum){
-    //             case 1:
-    //                 palo= "Picas";
-    //                 break;
-    //             case 2:
-    //                 palo= "Corazones";
-    //                 break;
-    //             case 3:
-    //                 palo= "Diamantes";
-    //                 break;
-    //             case 4:
-    //                 palo= "Treboles";
-    //                 break;
-    //         }
-    //         // Ver si la carta ya esta en el mazo
-    //         String paraBuscar=valor+palo;
-    //         // si ya esta se continua a la sig iteracion
-    //         if(cartasAgregadas.contains(paraBuscar)){
-    //             continue;
-    //         }
-    //         // si no se crea la carta y se agrega al mazo
-    //         Carta carta= new Carta(palo,valor);
-    //         mazo.add(0,carta);
-    //         cartasAgregadas+=","+valor+palo; // tambien al string 
-    //     }
-    //     return mazo; // Se regresa el mazo  ya completo
-    // } 
-
     /** Metodo para construir un mazoCompleto (SIN BARAJEAR),
      *  es decir con las 52 cartas de una baraja
      *  inglesa.
      * **/
     public TDAList<Carta> mazoCompleto(){
-         // Creamos la lista de cartas
-         TDAList<Carta> mazo= new DoubleLinkedList<>();
-         String palo="Picas";
+        // Creamos la lista de cartas
+        TDAList<Carta> mazo= new DoubleLinkedList<>();
+        int numPalo=0;
+        String[] posiblesPalosArr= {"Picas","Corazones","Diamantes","Treboles"};
 
-        //los siguientes fors son para agregar todas las cartas de cada palo
-
-         for(int i=1;i<=13;i++){
-           
-             Carta carta = new Carta(palo,i);
-             mazo.add(0,carta);
-         }
-
-
-          palo= "Treboles";
-         for(int i=1;i<=13;i++){
-            
-             Carta carta = new Carta(palo,i);
-             mazo.add(0,carta);
-         } 
-         palo= "Corazones";
-         for(int i=1;i<=13;i++){
-           
-             Carta carta = new Carta(palo,i);
-             mazo.add(0,carta);
-         }
-         
-         palo= "Diamantes";
-         for(int i=1;i<=13;i++){
-           
-             Carta carta = new Carta(palo,i);
-             mazo.add(0,carta);
-         } 
-
-
-         //Dianis no se xq no lo pude hacer en un solo for como que intentaba ir cambiando el valor de palo pero no se xq no me cambiaba el valor que raro :(.🥺
-         return mazo;
+        while(numPalo<4){  // Para que haga los distintos palos (0-3)
+            for(int i=1;i<=13;i++){ // para que se hagan los distintos valores (1-13)
+                Carta carta= new Carta(posiblesPalosArr[numPalo],i);
+                mazo.add(0,carta);
+            }
+            numPalo++; // Ya que creo todas las cartas del palo actual pasa al siguiente.        
+        
+        }
+        return mazo; //Regresamos el mazo ya completo 
     }
+   
          
-    /**Método para barajear el mazo completo 
-     * se creara una nueva lista donde pondremos las cartas en un orden aleatorio
+    /** Método para barajear el mazo completo 
+     *  se creara una nueva lista donde pondremos las cartas en un orden aleatorio
      */
     public TDAList<Carta> mazoCompletoBarajeado(){
         //lista no barajeada
@@ -192,18 +61,20 @@ public class Mazo{
                 mazoCompletoBarajeado.add(0,mazo.get(0));
                 break;
             }
-            //agregamos en la primera posicion algun elemento aleatorio de la lista mazo completa ya que remueve de valor devuelve un elemento aleatorio.
+            /*agregamos en la primera posicion algun elemento aleatorio
+             *de la lista mazo completa.
+             * usamos remove ya que removemos el valor de mazo(para que no se repitan) 
+             *  y se devuelve el elemento eliminado en la posicion aleatoria.*/
             mazoCompletoBarajeado.add(0,mazo.remove(valor));
-           // System.out.println(mazo.size());
+           
         }
-        
-        //System.out.println(mazoCompletoBarajeado.size());
         return mazoCompletoBarajeado;    
     }
+
     public static void main(String[] args){
         Mazo p1=new Mazo();
         System.out.println(p1.mazoCompleto()+ "\n");   
-        System.out.println(p1.mazoCompletoBarajeado());      
+        //System.out.println(p1.mazoCompletoBarajeado());      
     }
 
 }
