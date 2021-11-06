20fc15ffcabdae8036eaa1a91beb0dcad4fee0a0
DianaSalgado22 <dianasalgado@ciencias.unam.mx>
2021-11-05 19:41:47 -0700

    Cambios es clase carta y creación de clase Player y partida

---
+++ Proyecto1/src/fciencias/edatos/proyecto01/Partida.java
@@ -0,0 +1,27 @@
+package fciencias.edatos.proyecto01;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+/**
+ * @version 1.0 Noviembre 2021.
+ * @author Salgado Tirado Diana Laura
+ * @author Celic Aislinn Liahut Ley
+ * @since Estructuras de Datos 2022-1
+ * Clase que simula la partida del juego Solterona
+ */
+public class Partida{
+
+    // Cantidad de jugadores 
+    int cantPlayers;
+
+    /** Método para construir una partida
+     * 
+     */
+
+
+
+
+
+
+
+}
---
+++ Proyecto1/src/fciencias/edatos/proyecto01/Player.java
@@ -0,0 +1,20 @@
+package fciencias.edatos.proyecto01;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+/**
+ * @version 1.0 Noviembre 2021.
+ * @author Salgado Tirado Diana Laura
+ * @author Celic Aislinn Liahut Ley
+ * @since Estructuras de Datos 2022-1
+ * Clase que simula a un jugador del juego Solterona
+ */
+public class Player{
+    // Turno del jugador 
+    int playerTurn;
+    // Nombre del jugador
+    String name="";
+    // Mazo del jugador
+    Mazo mazoPlayer= new Mazo();
+
+}
