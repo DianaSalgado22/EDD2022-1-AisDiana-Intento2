f389bce1fed112400f509865754df150f1d377bb
DianaSalgado22 <dianasalgado@ciencias.unam.mx>
2021-12-22 14:05:57 -0700

    Metodo rotarDerecha

--- Practica06LiahutCelicSalgadoDiana/src/fciencias/edatos/practica06/AVLTree.java
+++ Practica06LiahutCelicSalgadoDiana/src/fciencias/edatos/practica06/AVLTree.java
@@ -271,6 +271,50 @@ public class AVLTree<K extends Comparable, T> implements TDABinarySearchTree<K, 
 	} */
 
 	public AVLNode rotarALaDerecha(AVLNode actual){
+		// Guardamos al papá del nodo actual
+		AVLNode aux=actual.padre;
+		// Definimos a y b
+		AVLNode a=actual.izquierdo;
+		AVLNode b=actual;
+		// Caso donde a es null (es decir el arbol quedaria igual)
+		if(a==null){
+			return actual;
+		}
+		// Convertimos a  en padre de b}
+		b.padre=a;
+		// Ahora el subarbol derecho de a es el subarbol izq de actual(b)
+		b.izquierdo=a.derecho;
+		// Falta decirle a el subarbol derecho que su padre ahora es b
+		// Para esto checamos si el subarbol derecho es null ya que de ser asi no se puede acceeder al padre de algo null
+		if(a.derecho!=null){
+			a.derecho.padre=b;
+		}
+		// Luego le tenemos que decir a que su hijo derecho es b
+		a.derecho=b;
+		
+		// Por ultimo tenemos que reconectar a la rotacion con el resto del arbol o en su defecto asignar a la nueva raiz
+		if(aux==null){ // Entonces b era la raiz
+			this.root=a; // cambiamos a como la nueva raiz
+			// Como ahora a es la raiz su padre tiene que ser null
+			a.padre=null
+		}
+		// Este es el caso cuando no se hizo una rotación en la raiz
+		else{
+			// el padre de b ahora es el padre de a
+			a.padre=aux;
+			// si actual era hijo derecho
+			if(aux.derecho==actual){
+				// entonces ahora su nuevo hijo derecho es a
+				aux.derecho=a;
+			}else{ // si acual era hijo izquierdo
+				// entonces ahora su nuevo hijo izq es a
+				aux.izquierdo=a;
+			}
+		}
+		return a; // La verdad no estoy segura de que es lo que se tendria que devolver en este
+		//Tampoco sé muy bien que onda con las alturas si se cambian aqui o no 
+		
+		/*
 		AVLNode nuevo = actual.izquierdo;
         actual.izquierdo = nuevo.derecho;
         nuevo.derecho= actual;
@@ -288,6 +332,7 @@ public class AVLTree<K extends Comparable, T> implements TDABinarySearchTree<K, 
 		// System.out.println(azul+"");
 		
 		// System.out.println(white+"");
+		*/
 	} 
 	
 
